d5727549a9823547a7611835e7829dfb9b3b10c7
jirenz747 <37732694+jirenz747@users.noreply.github.com>
2018-07-22 12:39:00 -0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,6 @@
 rm-f /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf 
 sudo /etc/init.d/nginx restart 
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test 
-sudo /etc/init.d/gunicorn restart 
 sudo /etc/init.d/mysql start
 sudo mkdir /home/box/web/public
 sudo mkdir /home/box/web/public/img
